c278a0fd901e2a968dda4d4b6f90d629a00a657d
Steve <shalomlandsman@gmail.com>
2018-10-31 15:05:51 -0400

    add react gmaps api dependency

---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactLoading from "react-loading";
+
+const Loading = ({ type, color }) => (
+    <div>fuck
+  <ReactLoading type={type} color={color} height={667} width={375} />
+  </div>
+);
+
+export default Loading;
--- src/rentalslist.js
+++ src/rentalslist.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import { Container } from "reactstrap";
 import Rental from "./Rental";
 import Search from "./Search";
+import Loading from './Loading';
 
 export default class RentalsList extends Component {
   render() {
     return (
       <Container>
         <Search />
-        {this.props.rentals.map(rental => (
+        {this.props.rentals[0] ? (this.props.rentals.map(rental => (
           <Rental key={rental._id} {...rental} />
-        ))}
+        ))): (
+          <Loading type={'balls'} color={'#ffffff'} />
+        ) 
+        }
       </Container>
     );
   }
